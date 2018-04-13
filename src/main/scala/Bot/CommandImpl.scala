fc2254b4908a68d819dc01897f623716ec33babb
morvell <zaxarov2702@yandex.ru>
2018-04-14 02:14:05 +0500

    maybe

--- src/main/scala/Bot/CommandImpl.scala
+++ src/main/scala/Bot/CommandImpl.scala
@@ -6,7 +6,7 @@ import java.util.Date
 import scala.collection.immutable
 
 trait Repository {
-  var polls: Map[Int, Poll] = immutable.Map[Int, Poll]()   //TODO go to trait repka ACCEPT
+  var polls: Map[Int, Poll] = immutable.Map[Int, Poll]() //TODO go to trait repka ACCEPT
 }
 
 object CommandImpl extends Repository {
@@ -15,39 +15,40 @@ object CommandImpl extends Repository {
   val formatDate = new SimpleDateFormat("hh:mm:ss yy:MM:dd")
 
 
-  def worker(string: String): Unit ={
+  def worker(string: String): Unit = {
     println(string)
   }
 
-  def createPoll(name : String, anonymityVar : Option[String], continuousOrAfterstopVar : Option[String],
-                 startTimeVar : Option[String], stopTimeVar : Option[String]): Unit = {
+  def createPoll(name: String, anonymityVar: Option[String], continuousOrAfterstopVar: Option[String],
+                 startTimeVar: Option[String], stopTimeVar: Option[String]): Int = {
     val anonymity = anonymityVar.getOrElse("yes") == "yes"
     val continuousOrAfterstop = continuousOrAfterstopVar.getOrElse("afterstop") == "continuous"
 
-    val startTime = formatDate.parse(startTimeVar.getOrElse(formatDate.format(new Date())))
-    val stopTime = formatDate.parse(stopTimeVar.getOrElse(formatDate.format(new Date())))
+    val startTime = formatDate.parse(startTimeVar.getOrElse(formatDate.format(new Date)))
+    val stopTime = formatDate.parse(stopTimeVar.getOrElse(formatDate.format(new Date)))
 
     val id = maxID
 
     maxID += 1
-    polls = polls + (id -> new Poll(name, id, anonymity, continuousOrAfterstop, startTime, stopTime))
+    polls = polls + (id -> Poll(name, id, anonymity, continuousOrAfterstop, startTime, stopTime))
 
-    println(id)
+    id
   }
 
-  def listPolls(): Unit = {
+  def listPolls(): String = {
     "Current polls: " + polls.foreach(x => println(x._1 + ": " + x._2.name)).toString
   }
 
-  def deletePoll(id : Int): Unit = { polls.contains(id) match {
-    case true => polls = polls - id
-                 println("Poll deleted successfully")
-    case false =>  println("Error: poll is not exist")
-  }
+  def deletePoll(id: Int): String = {
+
+    polls.get(id).map { (_) =>
+      polls = polls - id
+      "Poll deleted successfully"
+    }.getOrElse("Error: poll is not exist")
 
   }
 
-  def startPoll(id : Int): String = {
+  def startPoll(id: Int): String = {
     polls.get(id).map { poll =>
       poll.start()
       "The poll is started successfully"
@@ -56,18 +57,20 @@ object CommandImpl extends Repository {
   }
 
 
-  def stopPoll(id : Int): Unit = { polls.contains(id) match {
-    case true => polls(id).stop() //TODO make check start
-      println("The poll is stopped successfully")
-    case false => println("Error: poll is not exist") //TODO remove println
-  }
+  def stopPoll(id: Int): String = {
+
+    polls.get(id).map { poll =>
+      poll.stop()
+      "The poll is stopped successfully"
+    }.getOrElse("Error: poll is not exist")
+
   }
 
 
-  def pollResult(id : Int): Unit = { polls.contains(id) match {
-    case true =>  println(polls(id).getResult)
-    case false =>  println("Error: poll is not exist")
-  }
+  def pollResult(id: Int): String = {
+    polls.get(id).map { poll =>
+      polls(id).getResult
+    }.getOrElse("Error: poll is not exist")
   }
 
 }
--- src/main/scala/Bot/CommandParser.scala
+++ src/main/scala/Bot/CommandParser.scala
@@ -13,21 +13,21 @@ class CommandParser extends RegexParsers {
     val startTime = Parser("(" ~> """\d{2}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}""".r <~ ")")
     val stopTime = startTime
     ("/create_poll (" ~> pollName <~ ")") ~ anonymity.? ~ continuous.? ~ startTime.? ~ stopTime.? ^^
-      { case name ~ anonym ~ contin ~ start ~ stop  =>  command.createPoll( name, anonym, contin,
-        start, stop)}  //TODO case ~ ACCEPT
+      { case name ~ anonym ~ contin ~ start ~ stop  =>  command.worker(command.createPoll( name, anonym, contin,
+        start, stop).toString)}  //TODO case ~ ACCEPT
   }
 
-  def list: Parser[Unit] = """^/list""".r ^^ { _ => command.listPolls() }
-  def delete: Parser[Unit] = "/delete_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.deletePoll(d.toInt) } //TODO separate
-  def start: Parser[Unit] = "/start_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.startPoll(d.toInt) }
-  def stop: Parser[Unit] = "/stop_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.stopPoll(d.toInt) }
-  def result: Parser[Unit] = "/result (" ~> """\d+""".r <~ ")" ^^ { d => command.pollResult(d.toInt) }
+  def list: Parser[Unit] = """^/list""".r ^^ { _ => command.worker(command.listPolls()) }
+  def delete: Parser[Unit] = "/delete_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.worker(command.deletePoll(d.toInt)) } //TODO separate NEED HELP
+  def start: Parser[Unit] = "/start_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.worker(command.startPoll(d.toInt)) }
+  def stop: Parser[Unit] = "/stop_poll (" ~> """\d+""".r <~ ")" ^^ { d => command.worker(command.stopPoll(d.toInt)) }
+  def result: Parser[Unit] = "/result (" ~> """\d+""".r <~ ")" ^^ { d => command.worker(command.pollResult(d.toInt)) }
 
   def apply(input: String): Unit = parse(
     createPoll | list | delete | start | stop | result, input)
   match {
-    case Success(result,_) => result //TODO experement with Phrase IGNORE
-    case Failure(result, _) => println("Bad command: " + input)
+    case Success(result,_) => result
+    case Failure(result, _) => CommandImpl.worker("Bad command: " + input)
   }
 }
 
