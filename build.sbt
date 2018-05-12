28e8b56be31640ee4764fae8e51f387f522a651e
bottledeck <wivoceraku@p33.org>
2018-05-12 22:01:09 +0500

    Basik Telega)))

--- build.sbt
+++ build.sbt
@@ -9,3 +9,6 @@ libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % 
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+libraryDependencies += "info.mukel" %% "telegrambot4s" % "3.0.14"
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.5"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
--- src/main/scala/Bot/App.scala
+++ src/main/scala/Bot/App.scala
@@ -3,36 +3,28 @@ package Bot
 import java.util.Date
 
 import scala.io.Source
-import scala.util.Failure
 
 object App {
 
-  def main(args: Array[String]) {
-    val parser = CommandParser
+  def responseToLine(line: String): String = {
+    val result = CommandParser(line.trim)
 
-    val filename = "input.txt"
-
-    for (line <- Source.fromFile(filename).getLines) {
-      val result = parser(line.trim)
-
-      val prepared = result.map {
-          case c: Matcher.CreatePoolM => CommandImpl.createPoll(c.name,c.anon,c.cont,c.start,c.stop)
-          case c: Matcher.ListM => CommandImpl.listPolls()
-          case c: Matcher.DeleteM => CommandImpl.deletePoll(c.d)
-          case c: Matcher.StartM => CommandImpl.startPoll(c.d, new Date())
-          case c: Matcher.StopM => CommandImpl.stopPoll(c.d, new Date())
-          case c: Matcher.ResultM => CommandImpl.pollResult(c.d)
-      }
-
-      prepared match {
-        case parser.Success(response, _) => CommandImpl.worker(response.toString)
-        case parser.Failure(error, _) => CommandImpl.worker(s"Error: $error")
-      }
-
+    result.map {
+      case c: Matcher.CreatePoolM => CommandImpl.createPoll(c.name,c.anon,c.cont,c.start,c.stop)
+      case c: Matcher.ListM => CommandImpl.listPolls()
+      case c: Matcher.DeleteM => CommandImpl.deletePoll(c.d)
+      case c: Matcher.StartM => CommandImpl.startPoll(c.d, new Date())
+      case c: Matcher.StopM => CommandImpl.stopPoll(c.d, new Date())
+      case c: Matcher.ResultM => CommandImpl.pollResult(c.d)
+    } match {
+      case CommandParser.Success(response, _) => response.toString
+      case CommandParser.Failure(error, _) => s"Error: $error"
     }
   }
 
-
-
-
+  def main(args: Array[String]) {
+    for (line <- Source.fromFile("input.txt").getLines) {
+      println(responseToLine(line))
+    }
+  }
 }
